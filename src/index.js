bed3eb1c5eaa1eb27efbe0c4f44e74f33efe64ab
Sean Roberts <roberts.sean@gmail.com>
2016-11-07 15:50:53 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+// Vendor
+require('./react-ujs');
+
+import * as features from 'tenon-features';
+import { ResourceIndex, StandaloneList } from 'tenon-components';
+
+// Make jQuery requests identify themselves as json
+$.ajaxSetup({
+  dataType: 'json',
+  'beforeSend': function(xhr) {
+    xhr.setRequestHeader('Accept', 'application/json');
+  }
+});
+
+const Tenon = {
+  RI: {
+    'Root': ResourceIndex,
+    'StandaloneList': StandaloneList
+  },
+
+  modalHandlers: features.modalHandlers,
+
+  init: function() {
+    // init pickadate
+    $('[data-behavior=datepicker]').pickadate();
+    $('[data-behavior=timepicker]').pickatime();
+
+    // Set up modals
+    Tenon.modals = new features.ModalWindows();
+
+    // Mount components right away
+    window.ReactRailsUJS.mountComponents();
+
+    new features.AssetCropping();
+    new features.AssetDetachment();
+    new features.CocoonHooks();
+    new features.Dropdown();
+    new features.Expandable();
+    new features.Flash();
+    new features.FocusError();
+    new features.GenericClassToggler();
+    new features.I18nFields();
+    new features.ItemVersionAutosave();
+    new features.NavItemToggle();
+    new features.ProtectChanges();
+    new features.SortableNestedFields();
+    new features.Tabs();
+    new features.TenonContent();
+    new features.ToggleMainNav();
+
+    // Mount components again after features are done
+    window.ReactRailsUJS.mountComponents();
+  }
+};
+
+export default Tenon;
